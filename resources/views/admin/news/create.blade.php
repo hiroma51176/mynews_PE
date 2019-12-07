e206f398352dd3386e08a672f561a493231a7f50
hiroma <code51176@gmail.com>
2019-12-07 05:16:02 +0000

    mynews commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,16 +1,16 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
 
-{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィールの新規作成')
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
@@ -26,6 +26,6 @@
         <title>MyNews</title>
     </head>
     <body>
-        <!--<h1>プロフィール作成画面</h1>-->
+        <h1>Myニュース作成画面</h1>
     </body>
 </html>
