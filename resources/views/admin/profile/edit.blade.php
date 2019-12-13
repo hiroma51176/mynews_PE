0dac0c63ff88062c16d5fee8510d4fcd61a1c1e1
hiroma <code51176@gmail.com>
2019-12-13 04:19:24 +0000

    profile history update

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -51,7 +51,19 @@
                         </div>
                     </div>
                 </form>
-                
+                {{-- PHP/Laravel 17 課題で以下を追記 --}}
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>編集履歴</h2>
+                        <ul class="list-group">
+                            @if ($profile_form->profile_histories != NULL)
+                                @foreach ($profile_form->profile_histories as $history)
+                                    <li class="list-group-item">{{ $profile_history->edited_at }}</li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
